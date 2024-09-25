8369aa5912b5c96e7de76e56f6b1701d2aa6eb7b
Ângelo Cardoso <hoanngelos@gmail.com>
2024-09-24 23:58:39 -0300

    NavMenu adjustment.

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,17 +1,17 @@
 <template>
-  <nav class="border-gray-200 bg-gray-900">
+  <nav class="bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <div class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="@/assets/inventory.png" class="h-8" alt="Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Controle de Estoque</span>
       </div>
       <div class="w-full md:block md:w-auto">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-900">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
           <li>
-            <router-link to="/" class="block py-2 px-3 text-white bg-white rounded md:bg-transparent md:p-0">Estoque</router-link>
+            <router-link to="/" class="block py-2 px-3 text-white rounded md:p-0">Estoque</router-link>
           </li>
           <li>
-            <router-link to="/about" class="block py-2 px-3 text-white bg-white rounded md:bg-transparent md:p-0">Sobre</router-link>
+            <router-link to="/about" class="block py-2 px-3 text-white rounded md:p-0">Sobre</router-link>
           </li>
         </ul>
       </div>
